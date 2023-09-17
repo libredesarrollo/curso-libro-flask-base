5b535daae3119891d641970aaf64b724fa013a89
Andres Cruz <andrescruz@Mac-mini-de-Andres.local>
2023-09-17 05:58:53 -0400

    templates con Jinja

--- app/my_app/__init__.py
+++ app/my_app/__init__.py
@@ -1,16 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 from my_app.tasks.controllers import taskRoute
 
 from my_app.config import DevConfig
 
-app = Flask(__name__)
+app = Flask(__name__) #template_folder='/pages'
 app.register_blueprint(taskRoute)
 
 app.config.from_object(DevConfig)
 # app.debug = True
 
 @app.route('/')
-def hello_world() -> str:
+def hello_world(): # -> str
+    name = request.args.get('name','Desarrollolibre')
+    return render_template('index.html',task=name,name=name)
     return 'Hello Flask'
+
 
--- app/my_app/tasks/controllers.py
+++ app/my_app/tasks/controllers.py
@@ -1,10 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
 
-taskRoute = Blueprint('tasks',__name__,url_prefix='/',)
+taskRoute = Blueprint('tasks',__name__,url_prefix='/tasks',)
+
+task_list = ['tasks 1', 'tasks 2', 'tasks 3',]
 
 @taskRoute.route('/')
 def index():
-    return 'Index'
+    return render_template('dashboard/task/index.html', tasks=task_list)
 
 @taskRoute.route('/<int:id>')
 def show(id:int):
@@ -12,13 +14,26 @@ def show(id:int):
 
 @taskRoute.route('/delete/<int:id>')
 def delete(id:int):
-    return 'Delete '+str(id)
+    del task_list[id]
+    return redirect(url_for('tasks.index'))
 
 @taskRoute.route('/create', methods=('GET','POST'))
 def create():
-    return 'Create '
+    task = request.form.get('task')
+    # print(request.args.get('task'))
+    if task is not None:
+        task_list.append(task)
+        # return redirect('/tasks')
+        return redirect(url_for('tasks.index'))
+    return render_template('dashboard/task/create.html')
 
 @taskRoute.route('/update/<int:id>', methods=['GET','POST'])
 def update(id:int):
-    return 'Update '+str(id)
+
+    task = request.form.get('task')
+    if task is not None:
+        task_list[id] = task
+        return redirect(url_for('tasks.index'))
+
+    return render_template('dashboard/task/update.html')
 
